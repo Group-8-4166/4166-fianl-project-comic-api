54623e7d83fb7058fbbe926c8498578d0e3c0b85
ecolli47 <ecolli47@charlotte.edu>
2025-11-19 22:29:48 -0500

    comicCotroller error response update

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import helmet from "helmet";
+import cors from "cors";
 import morgan from "morgan";
 import swaggerUi from "swagger-ui-express";
 import YAML from "yamljs";
@@ -22,6 +23,7 @@ if (process.env.NODE_ENV === "development") {
   app.use(morgan("tiny"));
 }
 
+app.use(cors());
 app.use(helmet());
 app.use(express.json());
 
